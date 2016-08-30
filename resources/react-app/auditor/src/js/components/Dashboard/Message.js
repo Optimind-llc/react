f8bf15f163f77ff5bad8ef44ef23e9704cb8fb10
shiichi <s.shiichi311041@gmail.com>
2016-08-30 21:35:34 +0900

    modify conference cliant message sort and build

--- resources/react-app/auditor/src/js/components/Dashboard/Message.js
+++ resources/react-app/auditor/src/js/components/Dashboard/Message.js
@@ -80,7 +80,7 @@ class Message extends Component {
             }}
         >
         {
-          messages.map((m, i) =>
+          messages.sort((m1, m2) => m1.key > m2.key ? -1 : 1).map((m, i) =>
             <div className="message-node">
               <div
                 className="likes-wrap"
