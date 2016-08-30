306c7cb6eaf8ae9110ae930d5f719a027beac6b7
shiichi <s.shiichi311041@gmail.com>
2016-08-30 21:40:20 +0900

    modify conference cliant message sort and build

--- app/Http/Controllers/Conference/DashboardController.php
+++ app/Http/Controllers/Conference/DashboardController.php
@@ -64,7 +64,7 @@ class DashboardController extends Controller
             ->messages()
             ->with('likes')
             ->where('deleted_at', null)
-            ->orderBy('created_at')
+            ->orderBy('created_at', 'desc')
             ->get()
             ->map(function ($item, $key) {
                 return [
