3c2b6a9e697191fc56647dcccd1c48726312195c
shiichi <s.shiichi311041@gmail.com>
2016-09-01 16:42:00 +0900

    update line-chart and build

--- resources/react-app/auditor/src/js/components/Dashboard/MainContent.js
+++ resources/react-app/auditor/src/js/components/Dashboard/MainContent.js
@@ -23,7 +23,7 @@ class MainContent extends Component {
 
     this.state = {
       intervalId: null,
-      interval: 10000,
+      interval: 1000,
       innerHeight: window.innerHeight
     };
   }
